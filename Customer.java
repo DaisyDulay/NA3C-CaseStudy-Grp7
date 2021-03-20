2340ea49765c4716de2a9f6ce0981a001d51c0dd
GerrymeeTanedo <emyyy.tanedo@gmail.com>
2021-03-20 18:45:10 +0800

    method 11, 12 & 13

---
+++ Customer.java
@@ -0,0 +1,39 @@
+
+package mainbank;
+public class Customer extends Person{
+        static     String Address ;
+        static     String ContactNum;
+        static     String AccountNum;
+        static     String password;
+        static     String choice ;
+    
+        
+        //Gerrymee Tañedo "Method 11"  
+ 
+            public Customer(String spName){
+		super(spName);
+            }public void Address (String Address){
+                this.Address=Address;   
+            }public String Address(){
+                return Address;
+            } 
+        
+        //Gerrymee Tañedo "Method 12"
+
+             void Contact (String ContactNum){
+             this.ContactNum=ContactNum;
+            }public String Contact(){
+             return Address;
+            }  
+          
+        //Gerrymee Tañedo "Method 13"
+
+            void password(String password){
+            this.password=password;
+            }public String password(){
+            return password;
+            }     
+}
+      
+
+ 
